c995ec990eb296a1d5b72dfbccba227db9576e78
dragon friend <aliceaccounts@protonmail.com>
2022-08-06 00:18:35 -0400

    added a couple fizzbuzzes

---
+++ funcbuzz.c
@@ -0,0 +1,38 @@
+/*
+ * who needs strings when you've got __func__?
+ * works the same as squarebuzz except it uses __func__
+ */
+ #define Maine main
+ #include <stdio.h>
+ typedef void      __;
+ #define _         __func__
+ #define           __fucn__ for
+ #define           __func_  0xA7329CA00
+ #define            _func_  0xA6425
+ #define            _func__ printf
+ #define             func   FizzBuzz
+ #define           __fucn_  return
+;typedef int       __fucn
+;typedef long long __func
+;                 __ func
+(                    fucn
+){                 __fucn__
+(                  __func __
+,                    func__
+=1;                  func__
+<=                   fucn
+;                __=_func_
++                  __func_
+*!((                 func__
+%3)&&(               func__
+%5))+(1<<18)*!(      func__
+%15),                func__
++=!!                _func__
+(&                __,func__
+%3&&                 func__
+%5?                  func__
+:_+4*!!(             func__
+%3)));}            __fucn
+Maine(__){           func
+(20);              __fucn_
+(0);}
---
+++ squarebuzz.c
@@ -0,0 +1,23 @@
+/*
+ * fizzbuzz function written out as an 18*18 square of characters
+ * most of those zeros are for padding
+ */
+
+#include <stdio.h>
+void squarebuzz(n)
+<%for(unsigned lo\
+ng long _=!!01,_01
+,_02,_03;_01=!(_%(
+010-05))+(!!!(_%05
+)<<01),_02=((0000\
+000017236472502ULL
+&-((_01&(01*02))>>
+(!!_)))<<(010*04&-
+(_01==03)))+(0017\
+236464506&-(_01&01
+)),_03=0051631602\
+7045-(0-(00021503\
+7000&-!_01)),_<=+n
+;_+=!!printf((con\
+st char*)&_03,_01?
+(char*)&_02:_));%>
